ce515466c59409993522bafa0a4301bb4e22bb88
Weikeng Chen <chenweikeng27@gmail.com>
2017-09-30 20:45:07 +0800

    170930 add 0001 TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,30 @@
+import java.util.HashMap;
+
+
+
+//0001
+//170930
+
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer,Integer> map =  new HashMap<Integer,Integer>();
+        for(int i = 0;i < nums.length;i++)
+        {
+            int diff = target-nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff),i} ;
+            }
+            map.put(nums[i],i);
+        }
+        
+        return null;
+        
+    }
+}
+
+/*
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,8 +42,8 @@ public class main {
          
          scanner.close();
      }
-     
-     
+         
 }
+*/
 
 
